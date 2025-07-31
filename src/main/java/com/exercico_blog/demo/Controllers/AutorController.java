897ba9e8a1a48a9a5d17ec947a04f6d7db4ecdae
gabrielleber <gabriel.leber18Gmail.com>
2025-07-30 21:43:21 -0300

    atualizacao

--- src/main/java/com/exercico_blog/demo/Controllers/AutorController.java
+++ src/main/java/com/exercico_blog/demo/Controllers/AutorController.java
@@ -4,6 +4,7 @@ package com.exercico_blog.demo.Controllers;
 import com.exercico_blog.demo.Repositories.AutorRepositorio;
 import com.exercico_blog.demo.entities.Autor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,10 @@ public class AutorController {
 
     }
 
-    @GetMapping("/idAutor")
+    @GetMapping("/{idAutor}")
+    public Autor buscarAutor(@PathVariable Integer idAutor) {
+        return this.autorRepositorio.findBy(idAutor).get();
+    }
 }
 
 
